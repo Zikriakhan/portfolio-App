4dc25764369ddadca2ee60486ffe46681c565535
Zikriakhan <muhammadjanzikria@gmail.com>
2023-07-31 03:12:27 -0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.linkedin.com/feed/" target={"blank"}>
+          linkedin
+          </a>
+          <a href="https://github.com/Zikriakhan?tab=repositories" target={"blank"}>
+            Github
+          </a>
+          <a href="https://zikriakhan.000webhostapp.com/" target={"blank"}>
+          Portfolio
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
